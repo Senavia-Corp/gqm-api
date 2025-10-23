ba28b63b8e8c17aeb2f578f95c42487bf325db77
anessimp <anessimp@gmail.com>
2025-10-23 10:49:20 -0500

    [ADDED] Minor changes

--- src/models/SupplierModel.py
+++ src/models/SupplierModel.py
@@ -17,25 +17,15 @@ class SupplierBase(SQLModel):
 
 
 class Supplier(SupplierBase, table=True):
-    __tablename__ = "supplier"
+    __tablename__ = "Supplier"
     # ID opcional porque Postgre le puede dar un id si la persona no lo digita.
     ID_Supplier: Optional[str] = Field(default=None, primary_key=True)
     # AQUI VAN LAS RELACIONES----------
-    # Crear funcion que autogenere un str dependiendo del trabajo (con un formato en especifico. ej: QID122025)
 
 
 class SupplierCreate(SupplierBase):
     pass
 
 
-class SupplierUpdate(SQLModel):
+class SupplierUpdate(SupplierBase):
     Company_Name: Optional[str] = Field(default=None)
-    Company_Website: Optional[str] = Field(default=None)
-    Description: Optional[str] = Field(default=None)
-    Acc_Status: Optional[str] = Field(default=None)
-    Acc_Rep: Optional[str] = Field(default=None)
-    Speciality: Optional[str] = Field(default=None)
-    Email_Address: Optional[str] = Field(default=None)
-    Coverage_Area: Optional[str] = Field(default=None)
-    Phone_Number: Optional[str] = Field(default=None)
-    Address: Optional[str] = Field(default=None)
