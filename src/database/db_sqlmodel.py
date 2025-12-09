4bd3ecbb0426f7da6603ce5ca3f54c6fb70afd4b
anessimp <anessimp@gmail.com>
2025-12-08 19:01:02 -0500

    [ADD] Modelos a database

--- src/database/db_sqlmodel.py
+++ src/database/db_sqlmodel.py
@@ -6,14 +6,18 @@ from decouple import config
 from src.models.AttachmentsModel import Attachments
 # from src.models.ChangeOrderModel import ChangeOrder
 from src.models.ClientModel import Client
+from src.models.EstimateCostModel import EstimateCost
 from src.models.JobModel import Job
 from src.models.MemberModel import Member
 from src.models.MultiplierRModel import MultiplierR
+from src.models.OrderModel import Order
 from src.models.PropertyManagerModel import PropertyManager
 from src.models.PropertyMgmtCoModel import PropertyMgmtCo
 from src.models.SkillsModel import Skills
 from src.models.SubcontractorModel import Subcontractor
 from src.models.SupplierModel import Supplier
+from src.models.TasksModel import Tasks
+from src.models.TechnicianModel import Technician
 # Modelos de links de las relaciones N:M
 from src.models.link_models.ClientPManager import ClientPrManagerLink
 from src.models.link_models.JobMember import JobMemberLink
