d5fcfad5ba649d5b3cf8fc5237887484bb7d3f47
Jeferson Forero Galvis <forerodeveloper@MacBook-Pro-de-Jeferson.local>
2026-05-21 16:34:01 -0500

    fix: se soluciona tema de webhooks error

--- main.py
+++ main.py
@@ -94,8 +94,9 @@ def create_app():
         allowed_origins.extend([o.strip() for o in env_origins.split(",") if o.strip()])
 
     # Configurar CORS con origines específicos
-    CORS(app, resources={r"/*": {"origins": allowed_origins}}, supports_credentials=True)
-
+    # CORS(app, resources={r"/*": {"origins": allowed_origins}}, supports_credentials=True)
+    # CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+    CORS(app)
     # Middleware de logs para todas las rutas
     register_request_logger(app)
 
